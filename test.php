ad8d90a8081cfbc85c9a26980a17ffb563abef62
xiang.liu <402910017@qq.com>
2017-11-16 15:01:04 +0800

    test

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require_once('page.class.php'); //分页类
 $showrow = 10; //一页显示的行数
 $curpage = empty($_GET['page']) ? 1 : $_GET['page']; //当前的页,还应该处理非数字的情况
 $url = "?page={page}"; //分页地址，如果有检索条件 ="?page={page}&q=".$_GET['q']
-//省略了链接mysql的代码，测试时自行添加
+
 $sql = "SELECT * FROM orders";
 $total = mysql_num_rows(mysql_query($sql)); //记录总条数
 if (!empty($_GET['page']) && $total != 0 && $curpage > ceil($total / $showrow))
